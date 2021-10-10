12720bc3136edff1052563cb79cfc36a6b08f955
siddhi1907 <kadamsiddhi95@gmail.con>
2021-10-10 21:49:27 +0530

    Test pipeline

--- scripts/template.sh
+++ scripts/template.sh
@@ -4,5 +4,5 @@ set -e
 GOCD_URL=${GOCD_URL:-gocd.example.com}
 
 
-curl -s -k "https://$GOCD_URL/go/api/admin/pipelines/Test-Pipeline" \
+curl -s -k "https://$GOCD_URL/go/api/admin/pipelines/simple-go-server" \
 -u 'goadmin:goadmin' -H 'Accept: application/vnd.go.cd.v11+json' | jq '. | {name: .name, stages: .stages}' > template.gocd.json
